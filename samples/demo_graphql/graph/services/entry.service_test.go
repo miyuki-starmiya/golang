629ab5cb1323fdb43b91473cd62fb9f126d9c8b8
StarmiyaMiyuki <sousaku_0912@yahoo.co.jp>
2023-11-11 14:55:49 +0900

    done test

--- samples/demo_graphql/graph/services/entry.service_test.go
+++ samples/demo_graphql/graph/services/entry.service_test.go
@@ -1,63 +1,70 @@
 package services_test
 
 import (
-	"io/ioutil"
-	"os"
 	"testing"
 
 	"demo_graphql/graph/model"
 	"demo_graphql/graph/services"
 )
 
-type MockConverter struct{}
+type (
+	mockConverter struct{}
+	mockRepository struct{}
+)
 
-func (mc *MockConverter) ConvertToEntries(fileContent string) []*model.Entry {
-	// Mock conversion logic
-	return []*model.Entry{
-		{ID: "1", /* other fields */},
-		{ID: "2", /* other fields */},
-	}
+var mockData []*model.Entry = []*model.Entry{
+	{
+		ID: "JP-5948372-B2",
+		Title: "音声アシスタントでの以前のユーザインタラクションに基づく意図導出",
+		Assignee: "アップル インコーポレイテッド, アップル  インコーポレイテッド",
+		Author: []string{"アダム ジョン チャイヤー", "アンドリュー ジョン ハーキンズ"},
+		PriorityDate: "2010-01-18",
+		CreationDate: "2014-06-20",
+		PublicationDate: "2016-07-06",
+		GrantDate: "2016-07-06",
+		ResultLink: "https://patents.google.com/patent/JP5948372B2/ja",
+		RepresentativeFigureLink: "",
+	},
+	// {ID: "2", /* other fields */},
 }
 
-func (mc *MockConverter) ConvertToJSON(entry *model.Entry) string {
+func (mc *mockConverter) ConvertToEntries(fileContent string) []*model.Entry {
+	return mockData
+}
+
+func (mc *mockConverter) ConvertToJSON(entry *model.Entry) string {
 	return ""
 }
 
+func (mr *mockRepository) Fetch() (string, error) {
+	return "", nil
+}
+
 func TestGetEntryByID(t *testing.T) {
-	// Setup
-	testData := `[{"id": "1", "name": "Test Entry 1"}, {"id": "2", "name": "Test Entry 2"}]`
-	tempFile, err := ioutil.TempFile("", "entry")
-	if err != nil {
-		t.Fatalf("Failed to create temp file: %v", err)
-	}
-	defer os.Remove(tempFile.Name())
-
-	_, err = tempFile.WriteString(testData)
-	if err != nil {
-		t.Fatalf("Failed to write to temp file: %v", err)
-	}
-	tempFile.Close()
-
-	// Create an instance of entryServiceImpl with a fake converter
 	service := services.NewEntryService(
-		&MockConverter{},
+		&mockConverter{},
+		&mockRepository{},
 	)
 
-	// Test cases
 	tests := []struct {
 		name    string
 		id      string
-		wantErr bool
+		want    *model.Entry
 	}{
-		{"EntryExists", "1", false},
-		{"EntryDoesNotExist", "3", true},
+		{"EntryExists", "JP-5948372-B2", mockData[0]},
+		{"EntryDoesNotExist", "xxx", &model.Entry{}},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			_, err := service.GetEntryByID(tt.id)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GetEntryByID() error = %v, wantErr %v", err, tt.wantErr)
+			got, err := service.GetEntryByID(tt.id)
+			// 異常系はpass
+			if err != nil {
+				return
+			}
+			// 正しいEntryを返す
+			if got != tt.want {
+				t.Errorf("GetEntryByID() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
